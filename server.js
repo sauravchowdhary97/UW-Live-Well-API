efdd6c47d6f3e8d2fe3ceda56c0fbc4776c69133
Saurav Chowdhary <schowdhary@wisc.edu>
2019-10-04 19:09:25 -0500

    Lots of additions for API calls

--- server.js
+++ server.js
@@ -123,13 +123,118 @@ app.post('/details', (req, res) => {
 	});
 });
 
+app.post('/addListing', (req, res) => {
+	const { state } = req.body;
+
+	db('listings')
+	.returning('*')
+	.insert(
+		{	email: state.email,
+			rent: state.rent,
+			rooms: state.rooms,
+			availibility: state.availibility,
+			pets: state.pets,
+			sharing: state.sharing,
+			propertyname: state.propertyName,
+			image: state.image,
+			joined: new Date()
+	})
+	.then(listing => {
+		console.log(listing);
+		if(listing[0].id)
+			res.json("successfully added");
+		else
+			res.status(400).json("failed to add");
+	})
+	.catch(err => {console.log(err)});
+})
+
+app.post('/searchListings', (req, res) => {
+	const { state } = req.body;
+	if(state===undefined)
+	{
+		db.select('*')
+		.from('listings')
+		.then(data => {
+			console.log(data);
+			res.json(data);
+		})
+		.catch(err => { console.log(err) } );
+	}
+	else
+	{
+		//console.log(state.minrent, state.maxrent, state.rooms);
+		db.select('*')
+		.from('listings')
+		.where('rent', '>=', state.minrent)
+		.andWhere('rent', '<=', state.maxrent)
+		.andWhere('rooms', '=', state.rooms)
+		.andWhere('sharing', '=', state.sharing)
+		.andWhere('pets', '=', state.pets)
+		.then(data => {
+			console.log(data);
+			if(data[0].id)
+				res.json(data);
+			else
+				res.json("zero results");
+		})
+		.catch(err => { console.log(err)})
+	}
+})
+
+app.post('/newFav', (req, res) => {
+	const { id, email } = req.body;
+	db('favlistings')
+	.returning('*')
+	.insert(
+		{	email: email,
+			listingid: id
+	})
+	.then(listing => {
+		if(listing[0].id)
+			res.json("successfully added");
+		else
+			res.status(400).json("failed to add");
+	})
+	.catch(err => {console.log(err)});
+})
+
+app.post('/getFavListings', (req, res) => {
+	const { email } = req.body;
+	let results = '';
+	db.select('*')
+	.from('favlistings')
+	.where('email', '=', email)
+	.then(data => {
+		console.log(data);
+		if(data[0].id)
+		{
+			results = data;
+			res.json(data);
+		}
+		else
+			res.json("zero");
+	})
+	.catch(err => {console.log(err)});
+})
+
+app.post('/getListing', (req, res) => {
+	const { id } = req.body;
+	db.select('*')
+	.from('listings')
+	.where('id', '=', id)
+	.then(data => {
+		console.log(data);
+		if(data[0].id)
+		{
+			res.json(data);
+		}
+		else
+			res.json("zero");
+	})
+	.catch(err => {console.log(err)});	
+})
+
 app.listen(3000, () => {
 	console.log("app is running");
 });
-
-/*
-	signin - post = success/fail
-	register - post = user
-	/profile/:userId --> GET = user
-
-*/
